e4cdc566c122f23bfc116ea6d82a90c0adf8b526
Turnerc7 <turnerc777@gmail.com>
2024-06-18 22:52:44 -0500

    Task 1 on Graded assignment 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,12 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+
 
 
 //TODO: Variables for Part 2
@@ -17,20 +18,29 @@ let candidateAnswers;
 
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-
+    // TODO 1.1b: Ask for candidate's name
+    const input = require('readline-sync');
+    let candidateName = input.question("What is your name?");
+    console.log("Hello, " + candidateName + "!");
 }
 
+// Call the function to prompt the user for their name
+askForName();
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  const input = require('readline-sync');
+candidateAnswer = input.question(question);
+console.log(candidateAnswer);
 
 }
-
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if(candidateAnswer) {
+  console.log("Good job! You are correct!");
+} else {
+  console.log("Sorry try again");
+}
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -42,10 +52,11 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+  candidateName = input.question("What is your name?")
+  console.log("Hello, " + candidateName + "!");
+}
   askQuestion();
   gradeQuiz(this.candidateAnswers);
-}
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
